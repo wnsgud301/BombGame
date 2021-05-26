ef307a49d0dea102bbbd8bc6cca4e67937088fbe
wnsgud301 <80800106+wnsgud301@users.noreply.github.com>
2021-05-26 11:22:57 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float translation;
-    private float rotation;
-
+    private float translation; private float rotation;
+    private Animator ani;
+    private void Start() { ani = GetComponent<Animator>(); }
     void Update()
     {
-        translation = Input.GetAxis("Vertical") * 10f;
-        rotation = Input.GetAxis("Horizontal") * 100f;
-
-        transform.Translate(0, 0, translation * Time.deltaTime);
-        transform.Rotate(0, rotation * Time.deltaTime, 0);
+        translation = Input.GetAxis("Vertical") * 10f; rotation = Input.GetAxis("Horizontal") * 100f;
+        transform.Translate(0, 0, translation * Time.deltaTime); transform.Rotate(0, rotation * Time.deltaTime, 0);
+        if (translation != 0 || rotation != 0) { ani.SetBool("walk", true); } else { ani.SetBool("walk", false); }
     }
+    private void OnCollisionEnter(Collision collision) { if (collision.collider.tag == "Obstacle") { ani.SetTrigger("damage"); } }
 }
