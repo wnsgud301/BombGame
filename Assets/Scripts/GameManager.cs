8ac5d057ffd52a9659160f6da4abe7455ce067ec
wnsgud301 <80800106+wnsgud301@users.noreply.github.com>
2021-06-02 11:30:59 +0900

    123

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    public bool isGameover = false;
+    public Text scoreText;
+    public GameObject gameoverUI;
+    private int score = 0;
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("두 개 이상의 게임 매니저가 존재합니다!");
+            Destroy(gameObject);
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isGameover && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Desert");
+        }
+        if (isGameover && Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
     }
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        if (score < 0)
+        {
+            OnPlayerDead();
+        }
+        scoreText.text = "Score : " + score;
+    }
+    public void OnPlayerDead()
+    {
+        isGameover = true;
+        gameoverUI.SetActive(true);
+    }
+    
+
 }
+
+
+
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,5 +13,13 @@ public class PlayerController : MonoBehaviour
         transform.Translate(0, 0, translation * Time.deltaTime); transform.Rotate(0, rotation * Time.deltaTime, 0);
         if (translation != 0 || rotation != 0) { ani.SetBool("walk", true); } else { ani.SetBool("walk", false); }
     }
-    private void OnCollisionEnter(Collision collision) { if (collision.collider.tag == "Obstacle") { ani.SetTrigger("damage"); } }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Enermy")
+        { ani.SetTrigger("attack01");
+            GameManager.instance.OnPlayerDead();
+        }
+    }
 }
+
+ 
